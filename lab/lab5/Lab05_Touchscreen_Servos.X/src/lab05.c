0045e6078bbbdb16c4be961ef633cef174281559
zoolzool7509 <68385452+zoolzool7509@users.noreply.github.com>
2022-07-06 22:59:09 +0200

    Add files via upload
    New file

--- lab/lab5/Lab05_Touchscreen_Servos.X/src/lab05.c
+++ lab/lab5/Lab05_Touchscreen_Servos.X/src/lab05.c
@@ -9,46 +9,6 @@
 #include "lcd.h"
 #include "led.h"
 
-
-void servo_init(){
-    
-    //setup OC8 (X axis)
-    CLEARBIT(TRISDbits.TRISD7); // Set OC8 as output
-    OC8R = 300; // Set the initial duty cycle to 0ms
-    OC8RS = 300; // Load OCRS: next pwm duty cycle
-    OC8CON = 0x0006; // Set OC8: PWM, no fault check, Timer2
-    SETBIT(T2CONbits.TON); // Turn Timer 2 off
-    
-    //setup OC7 (Y axis)
-    CLEARBIT(TRISDbits.TRISD6); // Set OC7 as output
-    OC7R = 300; // Set the initial duty cycle to 0ms
-    OC7RS = 300; // Load OCRS: next pwm duty cycle
-    OC7CON = 0x0006; // Set OC7: PWM, no fault check, Timer2
-    SETBIT(T2CONbits.TON); // Turn Timer 2 off  
-}
-
-void servo_control (int axis, float duty_cycle){ 
-
-    // axis X = 0, axis Y = 1
-    // duty_cycle (ms)
-    int OC_Value = (int)(duty_cycle * 4000.0 / 20.0);
-    lcd_printf("%d",OC_Value);       
-    if (axis == 0){  
-        //CLEARBIT(TRISDbits.TRISD7); // Set OC8 as output
-        //OC8R = OC_Value; // Set the initial duty cycle to 0ms
-        OC8RS = OC_Value; // Load OCRS: next pwm duty cycle
-        OC8CON = 0x0006; // Set OC8: PWM, no fault check, Timer2
-        //SETBIT(T2CONbits.TON); // Turn Timer 2 on
-        
-    } else {
-        //CLEARBIT(TRISDbits.TRISD6); // Set OC7 as output
-        //OC7R = OC_Value; // Set the initial duty cycle to 0ms
-        OC7RS = OC_Value; // Load OCRS: next pwm duty cycle
-        OC8CON = 0x0006; // Set OC8: PWM, no fault check, Timer2
-        //SETBIT(T2CONbits.TON); // Turn Timer 2 on
-    }
-}
-
 #define TCKPS_1   0x00
 #define TCKPS_8   0x01
 #define TCKPS_64  0x02
@@ -59,59 +19,201 @@ void servo_control (int axis, float duty_cycle){
 #define PWM_MAX_US 2000
 #define PWM_CYC_US 20000
 
-void touch_init(){
+void Servo_Init(){
+    
+	TRISDbits.TRISD7 = 0; //Set 0C8 as output
+	TRISDbits.TRISD6 = 0; //Set 0C7 as output
+	
+	//Initialize 0C8 (X axis)
+	OC8CONbits.OCM = 0b000; // Disable Output Compare Module
+	OC8R = 300; // Write the duty cycle for the first PWM pulse (Duty 1.5 ms)
+	OC8RS = 300; // Write the duty cycle for the second PWM pulse (Duty 1.5 ms)
+	OC8CONbits.OCTSEL = 0; // Select Timer 2 as output compare time base
+	OC8CONbits.OCM = 0b110; // Select the Output Compare mode
 
-CLEARBIT(TRISEbits.TRISE1); // I/O pin set to output
-CLEARBIT(TRISEbits.TRISE2); // I/O pin set to output
-CLEARBIT(TRISEbits.TRISE3); // I/O pin set to output
+	//Initialize 0C7 (Y axis)
+	OC8CONbits.OCM = 0b000; // Disable Output Compare Module
+	OC8R = 300; // Write the duty cycle for the first PWM pulse (Duty 1.5 ms)
+	OC8RS = 300; // Write the duty cycle for the second PWM pulse (Duty 1.5 ms)
+	OC8CONbits.OCTSEL = 0; // Select Timer 2 as output compare time base
+	OC8CONbits.OCM = 0b110; // Select the Output Compare mode
+
+	// Initialize and enable Timer2
+	T2CONbits.TON = 0; // Disable Timer
+	T2CONbits.TCS = 0; // Select internal instruction cycle clock
+	T2CONbits.TGATE = 0; // Disable Gated Timer mode
+	T2CONbits.TCKPS = 0b00; // Select 1:1 Prescaler
+	TMR2 = 0x00; // Clear timer register
+	PR2 = 4000; // Load the period value, 20 ms  = 0.02 * 12.8 * 1,000,000 / 64
+	IPC1bits.T2IP = 0x01; // Set Timer 2 Interrupt Priority Level
+	IFS0bits.T2IF = 0; // Clear Timer 2 Interrupt Flag
+	IEC0bits.T2IE = 0; // Disable Timer 2 interrupt
+	T2CONbits.TON = 1; // Start Timer
+	
+}
+
+void Servo_Control (int axis, float duty_cycle){ 
+
+    // Input1: axis X = 0, axis Y = 1
+    // Input2: duty_cycle (ms)
+	
+    int OC_Value = (int)(duty_cycle * 4000.0 / 20.0);
+    lcd_printf("%d",OC_Value);       
+    if (axis == 0){  
+	
+        OC8RS = OC_Value; // Load OCRS: next pwm duty cycle  
+		
+    } else {
+
+        OC7RS = OC_Value; // Load OCRS: next pwm duty cycle
+    }
+}
+
+void Touch_Init(){
+
+		CLEARBIT(TRISEbits.TRISE1); // I/O pin set to output
+		CLEARBIT(TRISEbits.TRISE2); // I/O pin set to output
+		CLEARBIT(TRISEbits.TRISE3); // I/O pin set to output
 
 }
 
-unsigned int touch_output(int axis){
+void Touch_Standby(){
+        
+        SETBIT(PORTEbits.RE1);
+        SETBIT(PORTEbits.RE2);
+        CLEARBIT(PORTEbits.RE3);
+		
+}
+
+unsigned int Touch_Read(int axis){
+
+	// input: X asix (0) or Y axis (1)
+	// output: Analog pin
 
     if(axis == 0){  // read X axis
-        CLEARBIT(PORTEbits.RE1);
+ 
+		CLEARBIT(PORTEbits.RE1);
         SETBIT(PORTEbits.RE2);
         SETBIT(PORTEbits.RE3);
         
-        return (PORTBbits.RB15);  //AN15
-    } else {
+        return ADC_Read(0);  //read ADC for AN15
+    
+	} else {
         // read Y axis
         SETBIT(PORTEbits.RE1);
         CLEARBIT(PORTEbits.RE2);
         CLEARBIT(PORTEbits.RE3);
         
-        return (PORTBbits.RB9);  //AN9
-    }
-       
-        
-}
-
-unsigned int touch_standby(){
-        
-        SETBIT(PORTEbits.RE1);
-        SETBIT(PORTEbits.RE2);
-        CLEARBIT(PORTEbits.RE3);
-}
-
-void timer_init()
-{
-    //__builtin_write_OSCCONL(OSCCONL | 2); // Disable Timer
-    CLEARBIT(T2CONbits.TON); // Disable Timer
-    CLEARBIT(T2CONbits.TCS); // Select internal instruction cycle clock
-    CLEARBIT(T2CONbits.TGATE); // Disable Gated Timer mode
-    TMR2 = 0x00; // Clear timer register
-    T1CONbits.TCKPS = 0b10; // Select 1:64 Prescaler
-    CLEARBIT(IFS0bits.T2IF); // Clear Timer2 interrupt status flag
-    CLEARBIT(IEC0bits.T2IE); // Disable Timer2 interrupt enable control bit
-    PR2 = 4000; // 20 ms  = 0.02 * 12.8 * 1000000 / 64
+        return ADC_Read(1);  //read ADC for AN9
     
+	}    
 }
 
-void main_control(){
+void ADC_Init (){
+	
+	//Init. ADC for AN15
+	//disable ADC
+	CLEARBIT(AD1CON1bits.ADON);
+	//initialize PIN
+	SETBIT(TRISBbits.RB15); //set TRISB RB15 to input
+	CLEARBIT(AD1PCFGH.PCFG15); //set ADC1 pin AN20 as analog
+	//Configure AD1CON1
+	CLEARBIT(AD1CON1bits.AD12B) //set 10b resolution
+	AD1CON1bits.FORM = 0; //set integer output
+	AD1CON1bits.SSRC = 0x7; //set automatic conversion
+	//Configure AD1CON2
+	AD1CON2 = 0; //not using scanning sampling
+	//Configure AD1CON3
+	CLEARBIT(AD1CON3bits.ADRC); //internal clock source
+	AD1CON3bits.SAMC = 0x1F; //sample-to-conversion clock = 31Tad
+	AD1CON3bits.ADCS = 0x2; //Tad = 3Tcy (Time cycles)
+	//Leave AD1CON4 at its default value
+	//enable ADC
+	SETBIT(AD1CON1bits.ADON);
 
-servo_control(0, 1.5);  // 0.9 ms control X axis 0 degree
-servo_control(1, 1.5);  //0.9 mscontrol Y axis 0 degree
+
+	//Init. ADC for AN9
+	//disable ADC
+	CLEARBIT(AD2CON1bits.ADON);
+	//initialize PIN
+	SETBIT(TRISBbits.RB9); //set TRISB RB9 to input
+	CLEARBIT(AD2PCFGH.PCFG9); //set ADC2 pin AN9 as analog
+	//Configure AD1CON1
+	CLEARBIT(AD2CON1bits.AD12B) //set 10b resolution
+	AD2CON1bits.FORM = 0; //set integer output
+	AD2CON1bits.SSRC = 0x7; //set automatic conversion
+	//Configure AD1CON2
+	AD1CON2 = 0; //not using scanning sampling
+	//Configure AD1CON3
+	CLEARBIT(AD1CON3bits.ADRC); //internal clock source
+	AD2CON3bits.SAMC = 0x1F; //sample-to-conversion clock = 31Tad
+	AD2CON3bits.ADCS = 0x2; //Tad = 3Tcy (Time cycles)
+	//Leave AD1CON4 at its default value
+	//enable ADC
+	SETBIT(AD2CON1bits.ADON);
+
+}
+
+unsigned int ADC_Read (int axis){
+	
+	if(axis == 0){  
+	
+		// read X axis
+		AD1CHS0bits.CHS0SA = 0x00F; //set ADC1 to sample AN20
+		SETBIT(AD1CON1bits.SAMP); //start to sample
+		while(!AD1CON1bits.DONE); //wait for conversion to finish
+		CLEARBIT(AD1CON1bits.DONE); //MUST HAVE! clear conversion done bit
+        
+        return (ADC1BUF0);  //AN15
+    
+	} else {
+        // read Y axis
+		
+		AD2CHS0bits.CHS0SA = 0x009; //set ADC1 to sample AN9
+		SETBIT(AD2CON1bits.SAMP); //start to sample
+		while(!AD2CON1bits.DONE); //wait for conversion to finish
+		CLEARBIT(AD2CON1bits.DONE); //MUST HAVE! clear conversion done bit
+        
+        return (ADC2BUF0);  //AN9
+    
+	}
+
+}
+
+
+void main_task(){
+
+// step 1
+Touch_Standby();
+Servo_Control(0, 0.9);  // 0.9 ms control X axis 0 degree
+Servo_Control(1, 0.9);  //0.9 mscontrol Y axis 0 degree
+delay_ms(5000);
+lcd_printf("X-axis: %d, Y-axis: %d", Touch_Read(0), Touch_Read(1));
+lcd_locate(0, 3);
+
+// step 2
+Touch_Standby();
+Servo_Control(0, 0.9);  // 0.9 ms control X axis 0 degree
+Servo_Control(1, 2.1);  //0.9 mscontrol Y axis 0 degree
+delay_ms(5000);
+lcd_printf("X-axis: %d, Y-axis: %d", Touch_Read(0), Touch_Read(1));
+lcd_locate(0, 3);
+
+// step 3
+Touch_Standby();
+Servo_Control(0, 2.1);  // 0.9 ms control X axis 0 degree
+Servo_Control(1, 2.1);  //0.9 mscontrol Y axis 0 degree
+delay_ms(5000);
+lcd_printf("X-axis: %d, Y-axis: %d", Touch_Read(0), Touch_Read(1));
+lcd_locate(0, 3);
+
+// step 4
+Touch_Standby();
+Servo_Control(0, 2.1);  // 0.9 ms control X axis 0 degree
+Servo_Control(1, 0.9);  //0.9 mscontrol Y axis 0 degree
+delay_ms(5000);
+lcd_printf("X-axis: %d, Y-axis: %d", Touch_Read(0), Touch_Read(1));
+lcd_locate(0, 3);
 
 }
 
@@ -123,16 +225,18 @@ void main_loop()
     lcd_locate(0, 2);
     lcd_printf("Group: 1");
     
-    timer_init();
+    //timer_init();
     //touch_init();
-    servo_init();
+    Servo_Init();
+	ADC_Init();
+	Touch_Init();
     // initialize touchscreen
     
     // initialize servos
-    main_control(); 
+    
     while(TRUE) {
        
-       
+    main_task();   
         
     }
 }
